39813c195f01ef9455252b3a23fbcdf82881c0e0
shawnathanmcfly <sachimasi9187@gmail.com>
2018-12-16 13:00:39 -0500

    can now sign in

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,16 +11,20 @@ function displayUsers( ){
 
                 $('body').append(
                     
-                    "<div style='background-color:gray'>" + childSnap.val().username + "</div>"
+                    "<div style='background-color:gray'>" + childSnap.val().username + 
+                    "<img src='" + childSnap.val().userPhoto + "' />" +
+                    
+                    
+                    "</div>"
                 );
             });
         });
-        
     });
 }
 
 function signUp( email, user, bio, userPhoto, pass, age ) {
 
+    let errorData;
     let fread = new FileReader(), pData;
     
     fread.onload = (e) => { 
@@ -30,26 +34,64 @@ function signUp( email, user, bio, userPhoto, pass, age ) {
     fread.readAsDataURL( userPhoto.files[0] );
     firebase.auth().createUserWithEmailAndPassword(email, pass).catch( error =>  {
         
-        var errorCode = error.code;
+        errorData = error.code;
         var errorMessage = error.message;
-        console.log( errorMessage );
         
-    }).then( ()=> {
+        
+    }).then( () => {
 
         let uid = firebase.auth().currentUser.uid;
         console.log( pData );
         
-        firebase.database().ref( uid ).set( { age : age, username : user, bio : bio, userPhoto : pData })
+        firebase.database().ref( uid ).set( { age : age, username : user, bio : bio, userPhoto : pData, isOnline: true })
             .then(() => {
                 
-                displayUsers();
+                if( errorData === undefined )
+                    displayUsers();
                 
             })
             .catch(error => console.log("Error when creating user data.", error));
     });   
 }
 
+function login( email, pass ){
 
+    let errorData;
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+       
+        errorData = error.code;
+        
+        var errorMessage = error.message;
+        console.log(error.message);
+        
+      }).then( ()=> {
+
+        if( errorData === undefined ){
+            let uid = firebase.auth().currentUser.uid;
+            console.log(uid );
+
+            firebase.database().ref( uid ).onDisconnect().update({
+
+                isOnline : false
+        
+            });
+
+            firebase.database().ref( uid ).update({
+
+                isOnline : true
+        
+            });
+
+            firebase.database().ref( uid + "/userPhoto").on( 'value', snap => {  
+                $('body').append(
+
+                    "<img src='" + snap.val() + "' />"
+
+                );
+            });
+        }
+    });
+}
 
 window.onload = function(){
 
@@ -60,6 +102,12 @@ window.onload = function(){
             String( $("#bio").val() ), userImage, String( $("#password").val() ), $("#age").val() );
     });
 
-    
+    $('#sign-in').on( "click", (e) => {
+
+        e.preventDefault();
+        console.log("im clicked");
+        login( String( $("#inputEmail").val() ), String( $("#inputPassword").val() ) );
+    });
+
     
 }
