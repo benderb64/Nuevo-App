a4bfdbfd50cd3854ebb1d600896dca77c5d709fd
shawnathanmcfly <sachimasi9187@gmail.com>
2018-12-16 19:18:49 -0500

    direct to user bio on sign up or login

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+var uid;
 
 function displayUsers( ){
 
@@ -22,10 +23,34 @@ function displayUsers( ){
     });
 }
 
+function displayBio(){
+
+    $('body').html('');
+    firebase.database().ref( uid + "/" ).on( 'value', snap => {
+
+        $('body').append(
+            
+            '<div class="container">' +
+                '<div class="row">' +
+                    '<div id="eventTab" class="col-4 border-right border-bottom border-left bg-secondary"> Events </div>' +
+                    '<div id="messageTab" class="col-4 border-right border-bottom bg-secondary"> Messages </div>'+
+                    '<div id="bioTab" class="col-4 border-right bg-light"> Bio </div>' +
+                '</div>' +
+                '<div class="row">' +
+                    '<div class="col-4 bg-light"><img id="user-pic" src="' + snap.val().userPhoto +'" /></div>' +
+                    '<div class="col-8 bg-light"><h1>' + snap.val().username +'</h1><h3>Age: ' + snap.val().age + '</h3></div>' +
+                '</div>' +
+                '<div class="row">' +
+                    '<div class="col-12 bg-light"><p>' + snap.val().bio + '</p></div>' +
+                '</div>' +
+            '</div>'
+        );
+    });
+}
+
 function signUp( email, user, bio, userPhoto, pass, age ) {
 
-    let errorData;
-    let fread = new FileReader(), pData;
+    let fread = new FileReader(), pData, errorData;
     
     fread.onload = (e) => { 
         
@@ -40,14 +65,14 @@ function signUp( email, user, bio, userPhoto, pass, age ) {
         
     }).then( () => {
 
-        let uid = firebase.auth().currentUser.uid;
+        uid = firebase.auth().currentUser.uid;
         console.log( pData );
         
         firebase.database().ref( uid ).set( { age : age, username : user, bio : bio, userPhoto : pData, isOnline: true })
             .then(() => {
                 
                 if( errorData === undefined )
-                    displayUsers();
+                    displayBio();
                 
             })
             .catch(error => console.log("Error when creating user data.", error));
@@ -58,16 +83,13 @@ function login( email, pass ){
 
     let errorData;
     firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
-       
         errorData = error.code;
-        
-        var errorMessage = error.message;
         console.log(error.message);
         
       }).then( ()=> {
 
         if( errorData === undefined ){
-            let uid = firebase.auth().currentUser.uid;
+            uid = firebase.auth().currentUser.uid;
             console.log(uid );
 
             firebase.database().ref( uid ).onDisconnect().update({
@@ -81,15 +103,9 @@ function login( email, pass ){
                 isOnline : true
         
             });
-
-            firebase.database().ref( uid + "/userPhoto").on( 'value', snap => {  
-                $('body').append(
-
-                    "<img src='" + snap.val() + "' />"
-
-                );
-            });
         }
+
+        displayBio();
     });
 }
 
